9a5619ec9b720b81da8c1b3a4c141339643431d2
Sam Everett <severett29@Sams-Air.home>
2018-03-24 06:12:55 -0400

    first commit

---
+++ PictureProject/App.js
@@ -0,0 +1,64 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import {CameraKitCamera} from 'react-native-camera-kit';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+	    <CameraKitCamera
+	    ref={cam => this.camera = cam }
+	    style={{
+		    flex: 1,
+		    backgroundColor: 'white'
+		}}
+	    cameraOptions ={{
+		    flashMode: 'auto',
+		    focusMode: 'on',
+		    zoomMode: 'on',
+		    ratioOverlay: '1:1',
+		    ratioOverlayColor: '#00000077'
+		}}
+	    onReadQRCode={(event) => console.log(event.nativeEvent.qrcodeStringValue)}
+	    />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
